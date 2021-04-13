5dbb6b96c2713fac4d44c99180514e009da66a29
GeorgeII <egorzorikov@gmail.com>
2021-04-13 18:23:55 +0400

    Mix some (~15%) invalid payments while generating

--- src/main/scala/DataGenerator.scala
+++ src/main/scala/DataGenerator.scala
@@ -7,6 +7,7 @@ object DataGenerator {
 
   val numberOfFiles = 200
   val maxLinesNumber = 25000
+  val probabilityOfValidGeneration = 0.85
 
   val maxValue = 1000000
   val maxNameLength = 15
@@ -16,15 +17,51 @@ object DataGenerator {
 
   private val rnd = new Random
 
-  def generateName: String = {
+  def generateValidName: String = {
     val lengthOfName = rnd.nextInt(maxNameLength) + 1
     val name = rnd.alphanumeric.take(lengthOfName).mkString
 
     name
   }
 
-  def generateValue: Int = {
+  def generateValidValue: Int = {
     rnd.nextInt(maxValue) + 1
+  }
+
+  /**
+   * Even though the method does not guarantee to return an invalid name, it is highly likely
+   * to happen.
+   * @return String that contains ASCII characters from range 33-126.
+   */
+  def generateLikelyInvalidName: String = {
+    val lengthOfName = rnd.nextInt(maxNameLength) + 1
+    val name = (0 to lengthOfName).map(_ => rnd.nextPrintableChar).mkString
+
+    name
+  }
+
+  /**
+   * There's still a chance to generate a valid value. But it's very unlikely to happen.
+   * @return String of length 7 that contains ASCII chars from range 33-126.
+   */
+  def generateLikelyInvalidValue: String = {
+    rnd.alphanumeric.take(7).mkString
+  }
+
+  def generateValidPayment: String = {
+    val paymentFromName = generateValidName
+    val paymentToName   = generateValidName
+    val paymentValue    = generateValidValue
+
+    s"$paymentFromName->$paymentToName:$paymentValue"
+  }
+
+  def generateLikelyInvalidPayment: String = {
+    val paymentFromName = generateLikelyInvalidName
+    val paymentToName   = generateLikelyInvalidName
+    val paymentValue    = generateLikelyInvalidValue
+
+    s"$paymentFromName->$paymentToName:$paymentValue"
   }
 
   def writeFile(iteration: Int): Unit = {
@@ -34,11 +71,10 @@ object DataGenerator {
     val bw = new BufferedWriter(new FileWriter(file))
 
     for (line <- 0 until numberOfLines) {
-      val paymentFromName = generateName
-      val paymentToName   = generateName
-      val paymentAmount   = generateValue
+      // generate valid or invalid payment based on a given probability.
+      val isGenerateValid = rnd.nextDouble < probabilityOfValidGeneration
+      val payment = if (isGenerateValid) generateValidPayment else generateLikelyInvalidPayment
 
-      val payment = s"$paymentFromName->$paymentToName:$paymentAmount"
       bw.write(payment)
       bw.newLine()
     }
